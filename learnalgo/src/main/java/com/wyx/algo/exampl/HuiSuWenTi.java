ff6ff77b41703e71e68c18b5da88a0845644ef2f
王裕响 <wb-wyx657769@alibaba-inc.com>
2020-04-29 17:23:34 +0800

    add 回溯算法-N皇后问题

--- learnalgo/src/main/java/com/wyx/algo/exampl/HuiSuWenTi.java
+++ learnalgo/src/main/java/com/wyx/algo/exampl/HuiSuWenTi.java
@@ -25,12 +25,88 @@ import java.util.List;
 public class HuiSuWenTi {
 
     private static List<List<Integer>> res = new LinkedList<List<Integer>>();
+    private static List<char[][]> res2 = new LinkedList<char[][]>();
 
     public static void main(String[] args) {
-        int[] nums = new int[]{1, 2, 3};
-        List<List<Integer>> lists = quanPaiLei(nums);
-        System.out.println(lists);
+//        int[] nums = new int[]{1, 2, 3};
+//        List<List<Integer>> lists = quanPaiLei(nums);
+//        System.out.println(lists);
+        List<char[][]> lists = huangHou(8);
+        System.out.println(lists.size());
     }
+
+    /**
+     * N皇后问题，输入棋盘边长 n，返回所有合法的放置
+     * '.' 表示空，'Q' 表示皇后
+     */
+    private static List<char[][]> huangHou(int n) {
+        //初始化棋盘
+        char[][] cur = new char[n][n];
+        for(int i = 0;i < n;i++) {
+            for (int j = 0; j < n; j++){
+                cur[i][j] = '.';
+             }
+        }
+
+        backtrack2(cur, 0);
+        return res2;
+    }
+
+    // 路径：board 中小于 row 的那些行都已经成功放置了皇后
+    // 选择列表：第 row 行的所有列都是放置皇后的选择
+    // 结束条件：row 超过 cur 的最后一行
+    private static void backtrack2(char[][] cur, int row) {
+        //触发结束条件
+        if (row == cur.length) {
+            res2.add(cur);
+            return;
+        }
+        int n = cur[row].length;
+        for (int col = 0; col < n; col++) {
+            // 排除不合法选择
+            if (!isValid(cur, row, col)) {
+                continue;
+            }
+            // 做选择
+            cur[row][col] = 'Q';
+            // 进入下一行决策
+            backtrack2(cur,row + 1);
+            // 撤销选择
+            cur[row][col] = '.';
+        }
+    }
+
+    //是否可以在 cur[row][col] 放置皇后？
+    private static boolean isValid(char[][] cur, int row, int col) {
+        int n = cur.length;
+        //检查列是否有皇后冲突
+        for (int i = 0; i < n; i++) {
+            if (cur[i][col] == 'Q') {
+                return false;
+            }
+        }
+        //检查行是否有皇后冲突
+        for (int i = 0; i < cur[row].length; i++) {
+            if (cur[row][i] == 'Q') {
+                return false;
+            }
+        }
+        //检查右上方是否有皇后冲突
+        for (int i = row - 1, j = col + 1;
+             i >= 0 && j < n; i--, j++) {
+            if (cur[i][j] == 'Q') {
+                return false;
+            }
+        }
+        //检查左上方是否有皇后冲突
+        for (int i = row -1, j = col -1; i>=0 && j >= 0; i--, j--) {
+            if (cur[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     /**
      * 输入一组不重复的数字，返回它们的全排列
